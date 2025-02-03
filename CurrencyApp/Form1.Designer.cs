0be01fcbf4ffa8887b19eae9fa79764933af393c
nikita-laptev <159141822+nikita-laptev@users.noreply.github.com>
2025-02-03 23:25:40 +0500

    Base currency app

--- CurrencyApp/Form1.Designer.cs
+++ CurrencyApp/Form1.Designer.cs
@@ -2,15 +2,14 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboBoxFrom;
+        private System.Windows.Forms.ComboBox comboBoxTo;
+        private System.Windows.Forms.TextBox textBoxFrom;
+        private System.Windows.Forms.TextBox textBoxTo;
+        private System.Windows.Forms.Button buttonSwap;
+        private System.Windows.Forms.Label labelRate;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +19,70 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
-        }
+            this.comboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.comboBoxTo = new System.Windows.Forms.ComboBox();
+            this.textBoxFrom = new System.Windows.Forms.TextBox();
+            this.textBoxTo = new System.Windows.Forms.TextBox();
+            this.buttonSwap = new System.Windows.Forms.Button();
+            this.labelRate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
 
-        #endregion
+            // Настройка comboBoxFrom
+            this.comboBoxFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxFrom.FormattingEnabled = true;
+            this.comboBoxFrom.Location = new System.Drawing.Point(20, 20);
+            this.comboBoxFrom.Name = "comboBoxFrom";
+            this.comboBoxFrom.Size = new System.Drawing.Size(100, 21);
+
+            // Настройка textBoxFrom
+            this.textBoxFrom.Location = new System.Drawing.Point(20, 60);
+            this.textBoxFrom.Name = "textBoxFrom";
+            this.textBoxFrom.Size = new System.Drawing.Size(100, 20);
+
+            // Настройка comboBoxTo
+            this.comboBoxTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxTo.FormattingEnabled = true;
+            this.comboBoxTo.Location = new System.Drawing.Point(200, 20);
+            this.comboBoxTo.Name = "comboBoxTo";
+            this.comboBoxTo.Size = new System.Drawing.Size(100, 21);
+
+            // Настройка textBoxTo
+            this.textBoxTo.Location = new System.Drawing.Point(200, 60);
+            this.textBoxTo.Name = "textBoxTo";
+            this.textBoxTo.ReadOnly = true;
+            this.textBoxTo.Size = new System.Drawing.Size(100, 20);
+
+            // Настройка buttonSwap
+            this.buttonSwap.Location = new System.Drawing.Point(140, 20);
+            this.buttonSwap.Name = "buttonSwap";
+            this.buttonSwap.Size = new System.Drawing.Size(40, 23);
+            this.buttonSwap.Text = "⇄";
+            this.buttonSwap.UseVisualStyleBackColor = true;
+
+            // Настройка labelRate
+            this.labelRate.AutoSize = true;
+            this.labelRate.Location = new System.Drawing.Point(20, 100);
+            this.labelRate.Name = "labelRate";
+            this.labelRate.Size = new System.Drawing.Size(80, 13);
+            this.labelRate.Text = "Курс: 1 USD = ...";
+
+            // Настройка Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 140);
+            this.Controls.Add(this.labelRate);
+            this.Controls.Add(this.buttonSwap);
+            this.Controls.Add(this.textBoxTo);
+            this.Controls.Add(this.comboBoxTo);
+            this.Controls.Add(this.textBoxFrom);
+            this.Controls.Add(this.comboBoxFrom);
+            this.Name = "Form1";
+            this.Text = "Конвертер валют";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
 }
--- CurrencyApp/Form1.cs
+++ CurrencyApp/Form1.cs
@@ -1,10 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace CurrencyApp
 {
     public partial class Form1 : Form
     {
+        // Словарь с фиксированными курсами валют относительно доллара США (USD)
+        private Dictionary<string, decimal> exchangeRates = new Dictionary<string, decimal>
+        {
+            { "USD", 1.0m },   // Доллар США – базовая валюта
+            { "EUR", 0.85m },  // Евро
+            { "RUB", 90.0m },  // Российский рубль
+            { "GBP", 0.75m }   // Британский фунт
+        };
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent(); // Инициализация компонентов формы
+
+            // Подписка на события изменения данных в элементах управления
+            textBoxFrom.TextChanged += textBoxFrom_TextChanged;
+            comboBoxFrom.SelectedIndexChanged += comboBoxFrom_SelectedIndexChanged;
+            comboBoxTo.SelectedIndexChanged += comboBoxTo_SelectedIndexChanged;
+            buttonSwap.Click += buttonSwap_Click;
+        }
+
+        // Метод вызывается при загрузке формы
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Заполняем выпадающие списки валют
+            comboBoxFrom.Items.AddRange(exchangeRates.Keys.ToArray());
+            comboBoxTo.Items.AddRange(exchangeRates.Keys.ToArray());
+
+            // Устанавливаем валюты по умолчанию
+            comboBoxFrom.SelectedIndex = 0; // USD
+            comboBoxTo.SelectedIndex = 1;   // EUR
+
+            textBoxFrom.Text = "1"; // Начальная сумма для конвертации
+
+            UpdateConversion(); // Вызываем метод пересчёта валют
+        }
+
+        // Метод для пересчёта валют по курсу
+        private void UpdateConversion()
+        {
+            // Проверяем, является ли введённый текст числом
+            if (decimal.TryParse(textBoxFrom.Text, out decimal amount))
+            {
+                // Получаем выбранные валюты
+                string fromCurrency = comboBoxFrom.SelectedItem.ToString();
+                string toCurrency = comboBoxTo.SelectedItem.ToString();
+
+                // Рассчитываем курс: целевая валюта / исходная валюта
+                decimal rate = exchangeRates[toCurrency] / exchangeRates[fromCurrency];
+
+                // Пересчитываем сумму
+                decimal result = amount * rate;
+
+                // Выводим результат в текстовое поле (с округлением до двух знаков)
+                textBoxTo.Text = result.ToString("0.##");
+
+                // Обновляем метку курса
+                labelRate.Text = $"Курс: 1 {fromCurrency} = {rate:0.##} {toCurrency}";
+            }
+            else
+            {
+                // Если введено не число, выводим ошибку
+                textBoxTo.Text = "Ошибка";
+            }
+        }
+
+        // Обработчик события изменения текста в поле textBoxFrom
+        private void textBoxFrom_TextChanged(object sender, EventArgs e)
+        {
+            UpdateConversion(); // Пересчитываем валюту при изменении ввода
+        }
+
+        // Обработчик выбора новой валюты в comboBoxFrom
+        private void comboBoxFrom_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateConversion(); // Пересчитываем сумму
+        }
+
+        // Обработчик выбора новой валюты в comboBoxTo
+        private void comboBoxTo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateConversion(); // Пересчитываем сумму
+        }
+
+        // Обработчик кнопки "Поменять местами валюты"
+        private void buttonSwap_Click(object sender, EventArgs e)
+        {
+            // Меняем местами индексы валют
+            int tempIndex = comboBoxFrom.SelectedIndex;
+            comboBoxFrom.SelectedIndex = comboBoxTo.SelectedIndex;
+            comboBoxTo.SelectedIndex = tempIndex;
+
+            UpdateConversion(); // Пересчитываем сумму после смены валют
         }
     }
 }
